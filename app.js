6bcc405c1ccca470ef8669bc36bb7ab5ca817b0e
Reg-Sherman <reginaxsherman@gmail.com>
2021-12-16 14:39:03 -0600

    Commit 2 - Cube Calculation

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Part 1: Verbal Questions
+
 // 1. A parameter is essentially a placeholder for a value. 
     // The argument are the actual values that you want to pass in a function.
 
@@ -5,3 +7,14 @@
     // for us to visibly see what we're having the function do.
 
 // 3. The variable either has to be in the global scope or the local scope of the particular funtion.
+
+// Part 2: Calculate the Cube
+
+function calculateCube(number) {
+    return number ** 3
+};
+calculateCube(5);
+console.log(calculateCube(5));
+
+// Part 3: Is a Vowel?
+
