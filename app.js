9bc8e43a0433f505fe065ea7d42e90b262a8fdb7
Reg-Sherman <reginaxsherman@gmail.com>
2021-12-16 14:35:16 -0600

    Commit 1- Verbal Questions

---
+++ app.js
@@ -0,0 +1,7 @@
+// 1. A parameter is essentially a placeholder for a value. 
+    // The argument are the actual values that you want to pass in a function.
+
+// 2. Return tells the function want to do. Console.log is just a way
+    // for us to visibly see what we're having the function do.
+
+// 3. The variable either has to be in the global scope or the local scope of the particular funtion.
